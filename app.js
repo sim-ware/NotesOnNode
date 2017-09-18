03125774974e5fc5ac90853a64810d349908a254
Samir Gossain <samir.gossain@gmail.com>
2017-09-18 15:53:21 +0100

    Installs lodash

--- app.js
+++ app.js
@@ -2,9 +2,13 @@ console.log('Starting app.js');
 
 const fs = require('fs');
 const os = require('os');
+const _ = require('lodash');
 const notes = require('./notes.js');
 
-console.log('Result:', notes.addTime(11, -9));
+console.log(_.isString(true));
+console.log(_.isString('Samir'));
+
+// console.log('Result:', notes.addTime(11, -9));
 
 // var user = os.userInfo();
 //
